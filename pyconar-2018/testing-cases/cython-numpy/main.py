518d06fa60f6a86558dfea42a1dbaa7784f27fe5
Tomas Zulberti <tzulberti@gmail.com>
2018-11-22 22:07:11 -0300

    Deleted old files

--- pyconar-2018/testing-cases/cython-numpy/main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import numpy
-import pyximport
-
-pyximport.install(setup_args=dict(include_dirs=numpy.get_include()))
-
-import difference
-print(difference.__file__)
-difference.main()
